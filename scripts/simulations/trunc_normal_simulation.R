ae86b0680920109792772091d6290edd06173205
Aby369 <ingabyquintana@gmail.com>
2026-06-30 21:26:46 -0400

    Tests were added to check the quality of the generated variables.

--- scripts/simulations/trunc_normal_simulation.R
+++ scripts/simulations/trunc_normal_simulation.R
@@ -10,7 +10,7 @@ library(truncnorm)
 # Estableciendo semilla para reproducibilidad
 set.seed(1234)
 
-nsim <- 10^5
+nsim <- 1000
 U <- runif(nsim)
 # U = pnorm(Z, mean = 0, sd = 1) # caso dependiente
 
@@ -27,14 +27,36 @@ x<- qnorm(p*U+pnorm(-c), mean = 0, sd = 1)
 # X=G^(-1)(U)=(G^(-1)oF)(Z).
 # Normal truncada dependiente.
 
+# Histograma 
 hist(x, breaks = "FD", freq = FALSE, col = "lightblue", 
-     main = "Histograma datos con Normal Truncada(-2,2)") # Histograma de datos generados
-curve(dtruncnorm(x, a=-c, b=c), col = "darkred", add = TRUE)
+     main = "Histograma vs Normal Truncada(-2,2) Teórica",
+     xlab = "Valores de X", ylab = "Densidad")
+
+curve(dtruncnorm(x, a=-c, b=c, mean = 0, sd = 1), col = "darkred", add = TRUE)
+
+legend("topright", legend = c("Simulado (Hist)", "Teorico (Funcion)"), 
+       col = c("lightblue", "red"), lwd = 2, bty = "n")
+# 
+# # Ajuste de leyenda para reflejar el color del histograma (fill) y la línea (col)
+# legend("topright", legend = c("Simulado (Hist)", "Teórico (Función)"), 
+#        fill = c("lightblue", NA), border = c("black", NA),
+#        col = c(NA, "darkred"), lwd = c(NA, 2), bty = "n")
 
 
-# gnorm_trunc <- function(n,c) {
-#   X <- 
-#   U <- runif(n)
-#   p<- pnorm(c) - pnorm(-c)
-#   X<- qnorm(p*U[i]+pnorm(-c), mean = 0, sd = 1) 
-# }
+# Prueba de Kolmogorov-Smirnov (KS)
+# ------------------------------------------------------
+# H_0: Los datos siguen una distribucion Normal Truncada (-c,c)
+ks.test(x, "ptruncnorm", a = -2, b = 2, mean = 0, sd = 1)
+
+
+# Prueba de Ljung-Box (independencia)
+#------------------------------------------------------
+# H_0: Los datos son independientes (ruido blanco)
+Box.test(x, type = "Ljung-Box")
+
+# Gráfico de dispersion retardado
+plot(x[-length(x)], x[-1], xlab = "X_t", ylab = "X_t+1")
+
+# Correlacion
+cor(x[-length(x)], x[-1]) # -0.03698361
+
